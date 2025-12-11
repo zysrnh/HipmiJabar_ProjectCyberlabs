d470b27872dabf774aa008b40ab9535133292220
Hilmy Washil <hilmywashil@gmail.com>
2025-12-11 17:11:03 +0700

    Cuman nambah beberapa section di home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -58,7 +58,7 @@
     </section>
 
     <section class="misi">
-        <div class="yellow-accent"></div>
+        <div class="yellow-accent" style="align-self: center; !important"></div>
         <h2>Misi Kami</h2>
         <div class="misi-content">
             <div class="misi-text">
@@ -107,7 +107,227 @@
         </div>
     </section>
     <section class="pengumuman">
-        <div class="green-accent"></div>
+        <div class="green-accent" style="align-self: center; !important"></div>
         <h2>Pengumuman</h2>
+        <div class="pengumuman-content">
+            <div class="pengumuman-lastest">
+                <div class="pengumuman-banner-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="pengumuman-banner-bg">
+
+                    <div class="pengumuman-overlay"></div>
+
+                    <div class="pengumuman-banner-content">
+                        <!-- <span class="pengumuman-badge">Pengumuman</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="pengumuman-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="pengumuman-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+            </div>
+            <div class="pengumuman-others">
+                <div class="pengumuman-others-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="pengumuman-others-banner">
+
+                    <div class="pengumuman-others-content">
+                        <h2>HIPMI Banten Diminta Jawab Tantangan Besar Pengangguran</h2>
+                        <p class="pengumuman-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="pengumuman-others-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+                <div class="pengumuman-others-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="pengumuman-others-banner">
+
+                    <div class="pengumuman-others-content">
+                        <h2>HIPMI Banten Diminta Jawab Tantangan Besar Pengangguran</h2>
+                        <p class="pengumuman-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="pengumuman-others-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="strategic-plan">
+        <div class="strategic-plan-content">
+            <div class="strategic-plan-image">
+                <img src="{{ asset('images/strategic-plan1.png') }}" alt="Misi Image">
+            </div>
+            <div class="strategic-plan-wrapper">
+                <div class="green-accent" style="background-color: red; !important"></div>
+                <h1>Strategic Plan HIPMI JABAR</h1>
+                <div href="#" class="strategic-plan-cards">
+                    <!-- Looping satu disini -->
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <!-- Looping sampe sini -->
+                </div>
+            </div>
+        </div>
+        <div class="strategic-plan-content-reverse">
+            <div class="strategic-plan-image-reverse">
+                <img src="{{ asset('images/strategic-plan2.png') }}" alt="Misi Image">
+            </div>
+            <div class="strategic-plan-wrapper">
+                <div class="green-accent"></div>
+                <h1>Program dan Layanan</h1>
+
+                <div href="#" class="strategic-plan-cards">
+                    <!-- Looping satu disini -->
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <!-- Looping sampe sini -->
+
+                    <!-- Dummy -->
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <a href="#" class="strategic-plan-card">
+                        <h2>Tata Kelola organisasi</h2><i class="fa fa-arrow-right"></i>
+                    </a>
+                    <!-- Dummy -->
+                </div>
+            </div>
+    </section>
+    <section class="events">
+        <div class="green-accent" style="align-self: center; !important"></div>
+        <h2>Acara Kami di HIPMI</h2>
+        <div class="events-content">
+            <div class="events-lastest">
+                <div class="events-banner-card-lastest">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="events-banner-bg">
+
+                    <div class="events-overlay"></div>
+
+                    <div class="events-banner-content">
+                        <!-- <span class="events-badge">events</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="events-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="events-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+            </div>
+            <div class="events-others">
+                <div class="events-banner-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="events-banner-bg">
+
+                    <div class="events-overlay"></div>
+
+                    <div class="events-banner-content">
+                        <!-- <span class="events-badge">events</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="events-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="events-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+                <div class="events-banner-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="events-banner-bg">
+
+                    <div class="events-overlay"></div>
+
+                    <div class="events-banner-content">
+                        <!-- <span class="events-badge">events</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="events-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="events-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+                <div class="events-banner-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="events-banner-bg">
+
+                    <div class="events-overlay"></div>
+
+                    <div class="events-banner-content">
+                        <!-- <span class="events-badge">events</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="events-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="events-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+                <div class="events-banner-card">
+                    <img src="{{ asset('images/missions/mission-1.png') }}" class="events-banner-bg">
+
+                    <div class="events-overlay"></div>
+
+                    <div class="events-banner-content">
+                        <!-- <span class="events-badge">events</span> -->
+                        <h2>Presiden Joko Widodo Buka Rakernas HIPMI Ke-XVIII</h2>
+                        <p class="events-date">Oktober 28, 2025</p>
+
+                        <a href="#" class="events-btn-more">Lihat Lebih Banyak</a>
+                    </div>
+                </div>
+
+            </div>
+
+    </section>
+
+    <section class="ekatalog-home">
+        <div class="green-accent" style="align-self: center; !important"></div>
+        <h2>E-Katalog Bisnis HIPMI Jabar</h2>
+        <div class="e-katalog-content-home">
+            <!-- Looping dari sini cuy -->
+            <a href="{{ route('e-katalog.detail') }}">
+                <div class="katalog-card">
+                    <img src="{{ asset('images/hipmi-logo.png') }}">
+                    <div class="container">
+                        <h4><b>{{ Str::limit('Nama Perusahaan', 20, '...') }}</b></h4>
+                        <p>Bidang Perusahaan</p>
+                    </div>
+                </div>
+            </a>
+            <a href="{{ route('e-katalog.detail') }}">
+                <div class="katalog-card">
+                    <img src="{{ asset('images/hipmi-logo.png') }}">
+                    <div class="container">
+                        <h4><b>{{ Str::limit('Nama Perusahaan', 20, '...') }}</b></h4>
+                        <p>Bidang Perusahaan</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div style="margin-top: 50px;">
+            <a href="#" class="btn-ekatalog-home">Lihat Lebih Banyak</a>
+        </div>
+    </section>
+    <section class="daftarkan-bisnis-anda">
+        <img src="{{ asset('images/maju-babarengan.png') }}" alt="">
+        <h1>DAFTARKAN BISNIS ANDA</h1>
+        <a href="#" class="btn">Lihat Lebih Banyak</a>
+
     </section>
 @endsection
